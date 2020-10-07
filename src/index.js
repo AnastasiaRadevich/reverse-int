805d7e49eddce7fc46d521e413284a44e66038e7
Radevich <n.radievich@mail.ru>
2020-10-07 18:02:43 +0300

    feat: add task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let res = /[0-9]/g;
+    let num = n.toString().match(res).reverse()
+    num.forEach((item, index) => {
+        if (item === '0' && index === 0) {
+            num.splice(0, 1)
+        }
+    })
+    num = Number(num.join(''));
+    
+    return num;
 }
